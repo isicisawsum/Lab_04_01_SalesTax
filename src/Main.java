a0d678ce254ed39a8415ffe13c2e7020a12b8dfc
kregoric <75627074+isicisawsum@users.noreply.github.com>
2024-02-03 16:25:31 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        
         double salesTax = 0.05; //defining variable
+        
         double totalTax;//defining another variable
+        
         System.out.println("What is the price of your purchase?"); //output to 'prompt' user
+        
         int price = 15; //simulated input of 15
+        
         System.out.println("You inputted a price of $15"); // Showing what user 'inputted'
+        
         totalTax = (price * salesTax) + price; //calculating totalTax
+        
         System.out.println("The price of your item was $" + price + ", and there is a sales tax of 5%, bringing the total of the purchase to be $" + totalTax); //outputting the total tax
     }
 }
