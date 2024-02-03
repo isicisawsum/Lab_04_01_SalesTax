2b77af77a1100076c3d9224cb5ddc4d2cfac40f0
kregoric <isaac.a.kregor@gmail.com>
2024-02-03 11:53:44 -0500

    kregoric_SalesTax

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         double totalTax;//defining another variable
         System.out.println("What is the price of your purchase?"); //output to 'prompt' user
         int price = 15; //simulated input of 15
-        System.out.println("You inputted a price of $15");
+        System.out.println("You inputted a price of $15"); // Showing what user 'inputted'
         totalTax = (price * salesTax) + price; //calculating totalTax
         System.out.println("The price of your item was $" + price + ", and there is a sales tax of 5%, bringing the total of the purchase to be $" + totalTax); //outputting the total tax
     }
